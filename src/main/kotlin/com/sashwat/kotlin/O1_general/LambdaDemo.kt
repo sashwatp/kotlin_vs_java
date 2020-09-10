be4a1e1c6de87d2bb89868b4085e2ac111fa9b0f
Sashwat Prakash <sashwat.p@gmail.com>
2020-09-10 10:20:41 +0530

    Added notes around Kotlin filter to be eager

--- src/main/kotlin/com/sashwat/kotlin/O1_general/LambdaDemo.kt
+++ src/main/kotlin/com/sashwat/kotlin/O1_general/LambdaDemo.kt
@@ -2,8 +2,15 @@ package com.sashwat.kotlin.O1_general
 
 import java.util.stream.Collectors
 
+/**
+ *
+ * NOTE: In Kotlin these are not lazy operations as with Java Stream. 
+ */
 fun demo(myList: List<String>): List<String> {
+
     return myList.stream()
         .filter({ item -> item.contains("a") })
         .collect(Collectors.toList())
+
+
 }
